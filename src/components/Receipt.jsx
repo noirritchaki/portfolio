12bd5480ca1da089f71e2da07790776503fa2d1a
jonty <noirritchakii@gmail.com>
2026-03-15 14:11:19 +0530

    added a nowrap span for icons

--- src/components/Receipt.jsx
+++ src/components/Receipt.jsx
@@ -60,10 +60,13 @@ function Receipt() {
                 <tr key={index} className="receipt-row">
                   <td className="col-num">{String(index + 1).padStart(2, "0")}</td>
                   <td className="col-name">
-                    {item.name}
-                    <svg className="arrow-icon" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9C12 9.27614 11.7761 9.5 11.5 9.5C11.2239 9.5 11 9.27614 11 9L11 4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
-                    </svg>
+                    {item.name.split(" ").slice(0, -1).join(" ")}{" "}
+                    <span className="icon-wrap">
+                      {item.name.split(" ").slice(-1)[0]}
+                      <svg className="arrow-icon" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9C12 9.27614 11.7761 9.5 11.5 9.5C11.2239 9.5 11 9.27614 11 9L11 4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
+                      </svg>
+                    </span>
                   </td>
                   <td className="col-year">{item.year}</td>
                 </tr>
