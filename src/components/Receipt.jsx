2912d647e98af19785ed4b889b385cc7d02e49dd
jonty <noirritchakii@gmail.com>
2026-03-18 01:03:23 +0530

    added socials

--- src/components/Receipt.jsx
+++ src/components/Receipt.jsx
@@ -126,10 +126,28 @@ function Receipt() {
         <div className="receipt-divider"></div>
 
         <footer className="receipt-footer">
-          <p className="footer-line">github.com/noirritchaki</p>
-          <p className="footer-line">linkedin.com/in/noirritchaki</p>
-          <p className="footer-line">noirrit@email.com</p>
+          <div className="footer-socials">
+            <a href="https://www.linkedin.com/in/noirritchaki/" target="_blank" rel="noopener noreferrer" className="footer-social-link">
+              LINKEDIN
+              <svg className="arrow-icon" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9C12 9.27614 11.7761 9.5 11.5 9.5C11.2239 9.5 11 9.27614 11 9L11 4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
+              </svg>
+            </a>
+            <a href="https://x.com/jontychaki" target="_blank" rel="noopener noreferrer" className="footer-social-link">
+              X
+              <svg className="arrow-icon" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9C12 9.27614 11.7761 9.5 11.5 9.5C11.2239 9.5 11 9.27614 11 9L11 4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
+              </svg>
+            </a>
+            <a href="https://www.instagram.com/jontychaki" target="_blank" rel="noopener noreferrer" className="footer-social-link">
+              INSTAGRAM
+              <svg className="arrow-icon" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.64645 11.3536C3.45118 11.1583 3.45118 10.8417 3.64645 10.6465L10.2929 4L6 4C5.72386 4 5.5 3.77614 5.5 3.5C5.5 3.22386 5.72386 3 6 3L11.5 3C11.6326 3 11.7598 3.05268 11.8536 3.14645C11.9473 3.24022 12 3.36739 12 3.5L12 9C12 9.27614 11.7761 9.5 11.5 9.5C11.2239 9.5 11 9.27614 11 9L11 4.70711L4.35355 11.3536C4.15829 11.5488 3.84171 11.5488 3.64645 11.3536Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
+              </svg>
+            </a>
+          </div>
           <div className="receipt-divider"></div>
+          <p className="receipt-stars">* * *</p>
           <p className="receipt-thankyou">THANK YOU FOR VISITING!</p>
           <div className="receipt-barcode">
             ||| || ||| | || ||| || | ||| || ||| | ||
